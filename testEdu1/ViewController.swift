4782cfb7342f82203929d504a1f2f912764db411
Ilya <ilya@Ilyas-MacBook-Pro.local>
2024-01-17 11:45:33 +0400

    Label

--- testEdu1/ViewController.swift
+++ testEdu1/ViewController.swift
@@ -18,7 +18,13 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        let label = UILabel(frame: .zero)
+        label.text = "тестовый текст"
+        view.addSubview(label)
+        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
         // Добавь лэйбл по центру с текстом "тестовый текст"
     }
 }
